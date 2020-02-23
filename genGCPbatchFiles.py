73e72f7e04780a4a145788cf75b57e192d9307f3
kelley92@github.com <kelley92@bu.edu>
2020-02-23 16:12:33 -0500

    typos in gen bash files

--- genGCPbatchFiles.py
+++ genGCPbatchFiles.py
@@ -27,12 +27,12 @@ else:
 os.mkdir(out_path + 'gcp_batch_files')
 os.mkdir(out_path + 'logs')
 os.mkdir(out_path + 'errs')
-sh_file = open(out_path+'chirp-batch.sh','w')
+sh_file = open(out_path+'gcp_batch_files/chirp-batch.sh','w')
 sh_file.write('#!/bin/bash\n')
 
 for i, sec in enumerate(pt_cell.basal):
     file_name = str(sec)+'-batch.sbatch'
-    file = open(out_path+file_name,'w')
+    file = open(out_path+'gcp_batch_files/'+file_name,'w')
     
     file.write('#!/bin/bash\n')
     job_name = '#SBATCH --job-name=' + str(sec)+ '\n'
@@ -60,7 +60,7 @@ for i, sec in enumerate(pt_cell.basal):
 
 for i, sec in enumerate(pt_cell.apical):
     file_name = str(sec)+'-batch.sbatch'
-    file = open(out_path+file_name,'w')
+    file = open(out_path+'gcp_batch_files/'+file_name,'w')
     
     file.write('#!/bin/bash\n')
     job_name = '#SBATCH --job-name=' + str(sec)+ '\n'
@@ -69,16 +69,16 @@ for i, sec in enumerate(pt_cell.apical):
     file.write('#SBATCH -t 12:00:00\n')
     file.write('#SBATCH --nodes=1\n')
     file.write('#SBATCH --ntasks-per-node=1\n')
-    log_line = '#SBATCH -o /home/craig_kelley_downstate_edu/M1_Res/logs/'+str(sec)+'.log\n'
+    log_line = '#SBATCH -o ' + out_path + 'logs/' + str(sec) + '.log\n'
     file.write(log_line)
-    err_line = '#SBATCH -e /home/craig_kelley_downstate_edu/M1_Res/errs/'+str(sec)+'.err\n'
+    err_line = '#SBATCH -e ' + out_path + 'errs/' + str(sec) + '.err\n'
     file.write(err_line)
     if i == 0:
         file.write('#SBATCH --mail-user=craig.kelley@downstate.edu\n')
         file.write('#SBATCH --mail-type=end\n')
     file.write('source /home/craig_kelley_downstate_edu/.bashrc\n')
-    file.write('cd /home/craig_kelley_downstate_edu/M1_Res/\n')
-    run_line = 'mpirun -np 1 nrniv -python -mpi chirpForGCP.py ' + str(sec) + '\n'
+    file.write('cd /home/craig_kelley_downstate_edu/L5PYR_Resonance/\n')
+    run_line = 'mpirun -np 1 nrniv -python -mpi chirpForGCP.py ' + sys.argv[-1] + ' ' + str(sec) + '\n'
     file.write(run_line)
     file.close()
     

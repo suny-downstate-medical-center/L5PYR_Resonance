2ab223cc7e59c06fde1a69ec6034c20bc351cb49
kelley92@github.com <kelley92@bu.edu>
2020-05-01 11:33:26 -0400

    bare bones

--- varyLocalGs_hay.py
+++ varyLocalGs_hay.py
@@ -91,35 +91,35 @@ def varyLocalGs():
     # QfactorTrans.append(out['QfactorTrans'])
     # fVarIn.append(out['fVarIn'])
     # fVarTrans.append(out['fVarTrans'])
-    out = {}
+    # out = {}
 
-    freqs = out['Freq'][np.argwhere(out['ZinPhase'] > 0)]
-    if len(freqs) > 0:
-        ZinPeakPhaseFreq.append(out['Freq'][np.argwhere(out['ZinPhase'] == np.max(out['ZinPhase']))])
-        ZinLeadPhaseBW.append(freqs[-1] - freqs[0])
-        ZinLeadPhaseMinFreq.append(freqs[0])
-        ZinSynchFreq.append(freqs[-1])
-        ZinLeadPhaseBool.append(out['ZinPhase'] > 0)
-    else:
-        ZinPeakPhaseFreq.append(nan)
-        ZinLeadPhaseBW.append(0)
-        ZinLeadPhaseMinFreq.append(nan)
-        ZinSynchFreq.append(nan)
-        ZinLeadPhaseBool.append(out['ZinPhase'] > 0)
+    # freqs = out['Freq'][np.argwhere(out['ZinPhase'] > 0)]
+    # if len(freqs) > 0:
+    #     ZinPeakPhaseFreq.append(out['Freq'][np.argwhere(out['ZinPhase'] == np.max(out['ZinPhase']))])
+    #     ZinLeadPhaseBW.append(freqs[-1] - freqs[0])
+    #     ZinLeadPhaseMinFreq.append(freqs[0])
+    #     ZinSynchFreq.append(freqs[-1])
+    #     ZinLeadPhaseBool.append(out['ZinPhase'] > 0)
+    # else:
+    #     ZinPeakPhaseFreq.append(nan)
+    #     ZinLeadPhaseBW.append(0)
+    #     ZinLeadPhaseMinFreq.append(nan)
+    #     ZinSynchFreq.append(nan)
+    #     ZinLeadPhaseBool.append(out['ZinPhase'] > 0)
 
-    freqs = out['Freq'][np.argwhere(out['ZcPhase'] > 0)]
-    if len(freqs) > 0:
-        ZcPeakPhaseFreq.append(out['Freq'][np.argwhere(out['ZcPhase'] == np.max(out['ZcPhase']))])
-        ZcLeadPhaseBW.append(freqs[-1] - freqs[0])
-        ZcLeadPhaseMinFreq.append(freqs[0])
-        ZcSynchFreq.append(freqs[-1])
-        ZcLeadPhaseBool.append(out['ZcPhase'])
-    else:
-        ZcPeakPhaseFreq.append(nan)
-        ZcLeadPhaseBW.append(0)
-        ZcLeadPhaseMinFreq.append(nan)
-        ZcSynchFreq.append(nan)
-        ZcLeadPhaseBool.append(out['ZcPhase'])
+    # freqs = out['Freq'][np.argwhere(out['ZcPhase'] > 0)]
+    # if len(freqs) > 0:
+    #     ZcPeakPhaseFreq.append(out['Freq'][np.argwhere(out['ZcPhase'] == np.max(out['ZcPhase']))])
+    #     ZcLeadPhaseBW.append(freqs[-1] - freqs[0])
+    #     ZcLeadPhaseMinFreq.append(freqs[0])
+    #     ZcSynchFreq.append(freqs[-1])
+    #     ZcLeadPhaseBool.append(out['ZcPhase'])
+    # else:
+    #     ZcPeakPhaseFreq.append(nan)
+    #     ZcLeadPhaseBW.append(0)
+    #     ZcLeadPhaseMinFreq.append(nan)
+    #     ZcSynchFreq.append(nan)
+    #     ZcLeadPhaseBool.append(out['ZcPhase'])
         # print(str(stim_sec) + ' ' + str(loc))
     # else:
     #     for loc in np.linspace(1/(nseg+1), nseg/(nseg+1), nseg):
@@ -162,28 +162,28 @@ def varyLocalGs():
     #             ZcSynchFreq.append(nan)
     #             ZcLeadPhaseBool.append(out['ZcPhase'])
 
-    output = { 'ZinResAmp' : ZinResAmp,
-                'ZinResFreq' : ZinResFreq,
-                'ZcResAmp' : ZcResAmp,
-                'ZcResFreq' : ZcResFreq,
-                'dist' : dist,
-                'QfactorIn' : QfactorIn,
-                'QfactorTrans' : QfactorTrans,
-                'fVarIn' : fVarIn,
-                'fVarTrans' : fVarTrans,
-                'ZinPeakPhaseFreq' : ZinPeakPhaseFreq, 
-                'ZinLeadPhaseBW' : ZinLeadPhaseBW,
-                'ZinLeadPhaseMinFreq' : ZinLeadPhaseMinFreq,
-                'ZinSynchFreq' : ZinSynchFreq,
-                'ZinLeadPhaseBool' : ZinLeadPhaseBool,
-                'ZcPeakPhaseFreq' : ZcPeakPhaseFreq,
-                'ZcLeadPhaseBW' : ZcLeadPhaseBW,
-                'ZcLeadPhaseMinFreq' : ZcLeadPhaseMinFreq,
-                'ZcSynchFreq' : ZcSynchFreq,
-                'ZcLeadPhaseBool' : ZcLeadPhaseBool}
-
+    # output = { 'ZinResAmp' : ZinResAmp,
+    #             'ZinResFreq' : ZinResFreq,
+    #             'ZcResAmp' : ZcResAmp,
+    #             'ZcResFreq' : ZcResFreq,
+    #             'dist' : dist,
+    #             'QfactorIn' : QfactorIn,
+    #             'QfactorTrans' : QfactorTrans,
+    #             'fVarIn' : fVarIn,
+    #             'fVarTrans' : fVarTrans,
+    #             'ZinPeakPhaseFreq' : ZinPeakPhaseFreq, 
+    #             'ZinLeadPhaseBW' : ZinLeadPhaseBW,
+    #             'ZinLeadPhaseMinFreq' : ZinLeadPhaseMinFreq,
+    #             'ZinSynchFreq' : ZinSynchFreq,
+    #             'ZinLeadPhaseBool' : ZinLeadPhaseBool,
+    #             'ZcPeakPhaseFreq' : ZcPeakPhaseFreq,
+    #             'ZcLeadPhaseBW' : ZcLeadPhaseBW,
+    #             'ZcLeadPhaseMinFreq' : ZcLeadPhaseMinFreq,
+    #             'ZcSynchFreq' : ZcSynchFreq,
+    #             'ZcLeadPhaseBool' : ZcLeadPhaseBool}
+    output = {'val' : 0.5}
     # savemat('/u/craig/L5PYR_Resonance/Hay/Vary_Local_Gs/ih_' + sys.argv[-3] + '_im_' + sys.argv[-2] + '/' + stim_sec.name() + '.mat', output)
-    savemat('/u/craig/L5PYR_Resonance/Hay/Vary_Local_Gs/test.mat')
+    savemat('/u/craig/L5PYR_Resonance/Hay/Vary_Local_Gs/test.mat', output)
 
 def doNothing():
     x = 2 + 2

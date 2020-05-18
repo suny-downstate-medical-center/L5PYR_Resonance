04ced09dc6f8600f82063a82e1994d74197909e2
kelley92@github.com <kelley92@bu.edu>
2020-05-18 16:24:00 -0400

    combined data structure for both segments

--- sweepLagsMulti.py
+++ sweepLagsMulti.py
@@ -14,7 +14,7 @@ dSt = weight / (factor*10)
 Tp, _ = getTp(stim_seg, soma_seg, start, Sc0)
 
 data = getLagData(2, 0.5, Sc0, St0, dSt, start, Tp, 1, '/u/craig/L5PYR_Resonance/timeDomainOutput/HayApic2/')
-data = tuple(data)
+data1 = tuple(data)
 
 stim_seg = cell.apic[36](0.8)
 soma_seg = cell.soma[0](0.5)
@@ -26,17 +26,14 @@ St0 = weight / (factor*5)
 dSt = weight / (factor*10)
 Tp, _ = getTp(stim_seg, soma_seg, start, Sc0)
 
-data2 = getLagData(36, 0.5, Sc0, St0, dSt, start, Tp, 1, '/u/craig/L5PYR_Resonance/timeDomainOutput/HayApic36/')
+data2 = getLagData(36, 0.8, Sc0, St0, dSt, start, Tp, 1, '/u/craig/L5PYR_Resonance/timeDomainOutput/HayApic36/')
 data2 = tuple(data2)
 
-def mp_handler():
-    p = multiprocessing.Pool(4)
-    p.map(conditionAndTestMulti, data)
+data = (data1 + data2)
 
-def mp_handler2():
-    p = multiprocessing.Pool(4)
-    p.map(conditionAndTestMulti, data2)
+def mp_handler():
+    p = multiprocessing.Pool(6)
+    p.map(conditionAndTestMulti, data)
 
 if __name__ == '__main__':
     mp_handler()
-    mp_handler2()

9da1e46d4f0f16ddcabd82c706b8d629366f477d
kelley92@github.com <kelley92@bu.edu>
2020-05-21 13:43:26 -0400

    changed around to run 1 seg based on cmd line arg

--- sweepLagsMulti.py
+++ sweepLagsMulti.py
@@ -2,47 +2,51 @@ import multiprocessing
 from getCells import HayCell
 cell = HayCell()
 from synUtils import getLagData, getTp, conditionAndTestMulti
+import sys
 
-stim_seg = cell.apic[2](0.5)
-soma_seg = cell.soma[0](0.5)
-weight = 0.125
-start = 200
-factor = 4
-Sc0 = weight / factor
-St0 = weight / (factor*5)
-dSt = weight / (factor*10)
-Tp, _ = getTp(stim_seg, soma_seg, start, Sc0)
+if sys.argv[0] == '2':
+    stim_seg = cell.apic[2](0.5)
+    soma_seg = cell.soma[0](0.5)
+    weight = 0.125
+    start = 200
+    factor = 4
+    Sc0 = weight / factor
+    St0 = weight / (factor*5)
+    dSt = weight / (factor*10)
+    Tp, _ = getTp(stim_seg, soma_seg, start, Sc0)
 
-data = getLagData(2, 0.5, Sc0, St0, dSt, start, Tp, 1, '/u/craig/L5PYR_Resonance/timeDomainOutput/HayApic2/')
-data1 = tuple(data)
+    data = getLagData(2, 0.5, Sc0, St0, dSt, start, Tp, 1, '/u/craig/L5PYR_Resonance/timeDomainOutput/HayApic2/')
+    data = tuple(data)
 
-stim_seg = cell.apic[36](0.8)
-soma_seg = cell.soma[0](0.5)
-weight = 0.065
-start = 200
-factor = 4
-Sc0 = weight / factor
-St0 = weight / (factor*5)
-dSt = weight / (factor*10)
-Tp, _ = getTp(stim_seg, soma_seg, start, Sc0)
+elif sys.argv[0] == '36':
+    stim_seg = cell.apic[36](0.8)
+    soma_seg = cell.soma[0](0.5)
+    weight = 0.065
+    start = 200
+    factor = 4
+    Sc0 = weight / factor
+    St0 = weight / (factor*5)
+    dSt = weight / (factor*10)
+    Tp, _ = getTp(stim_seg, soma_seg, start, Sc0)
 
-data2 = getLagData(36, 0.8, Sc0, St0, dSt, start, Tp, 1, '/u/craig/L5PYR_Resonance/timeDomainOutput/HayApic36/')
-data2 = tuple(data2)
+    data = getLagData(36, 0.8, Sc0, St0, dSt, start, Tp, 1, '/u/craig/L5PYR_Resonance/timeDomainOutput/HayApic36/')
+    data = tuple(data)
 
-stim_seg = cell.apic[14](0.5)
-soma_seg = cell.soma[0](0.5)
-weight = 0.14
-start = 200
-factor = 4
-Sc0 = weight / factor
-St0 = weight / (factor*5)
-dSt = weight / (factor*10)
-Tp, _ = getTp(stim_seg, soma_seg, start, Sc0)
+elif sys.argv[0] == '14':
+    stim_seg = cell.apic[14](0.5)
+    soma_seg = cell.soma[0](0.5)
+    weight = 0.14
+    start = 200
+    factor = 4
+    Sc0 = weight / factor
+    St0 = weight / (factor*5)
+    dSt = weight / (factor*10)
+    Tp, _ = getTp(stim_seg, soma_seg, start, Sc0)
 
-data3 = getLagData(14, 0.5, Sc0, St0, dSt, start, Tp, 1, '/u/craig/L5PYR_Resonance/timeDomainOutput/HayApic14/')
-data3 = tuple(data3)
+    data = getLagData(14, 0.5, Sc0, St0, dSt, start, Tp, 1, '/u/craig/L5PYR_Resonance/timeDomainOutput/HayApic14/')
+    data = tuple(data)
 
-data = (data1 + data2 + data3)
+# data = (data1 + data2 + data3)
 
 def mp_handler():
     p = multiprocessing.Pool(6)

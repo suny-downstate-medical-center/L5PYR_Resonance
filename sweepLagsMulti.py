f8617b7104724a4d53c62b3f63295fef8ea2e44e
kelley92@github.com <kelley92@bu.edu>
2020-05-23 15:47:50 -0400

    run first apical trunk segment

--- sweepLagsMulti.py
+++ sweepLagsMulti.py
@@ -60,6 +60,20 @@ elif sys.argv[-1] == '409':
     data = getLagData(36, 0.14, Sc0, St0, dSt, start, Tp, 1, '/u/craig/L5PYR_Resonance/timeDomainOutput/HayApic409/')
     data = tuple(data)
 
+elif sys.argv[-1] == '0':
+    stim_seg = cell.apic[0](0.5)
+    soma_seg = cell.soma[0](0.5)
+    weight = 0.065
+    start = 200
+    factor = 4
+    Sc0 = weight / factor
+    St0 = weight / (factor*5)
+    dSt = weight / (factor*10)
+    Tp, _ = getTp(stim_seg, soma_seg, start, Sc0)
+
+    data = getLagData(0, 0.5, Sc0, St0, dSt, start, Tp, 1, '/u/craig/L5PYR_Resonance/timeDomainOutput/HayApic0/')
+    data = tuple(data)
+
 # data = (data1 + data2 + data3)
 
 def mp_handler():

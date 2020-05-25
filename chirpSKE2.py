686253cb4ffb0d6c2f7e78b370e2f1537ec5fec3
kelley92@github.com <kelley92@bu.edu>
2020-05-24 21:24:36 -0400

    classic typo

--- chirpSKE2.py
+++ chirpSKE2.py
@@ -6,7 +6,7 @@ def chirpForMulti(invar):
     sec_num, loc, filename = invar
     from getCells import HayCell
     pt_cell = HayCell()
-    sec = pt_cell.apic[sec_num](loc)
+    seg = pt_cell.apic[sec_num](loc)
     from neuron import h
     for sec in h.allsec():
         try: sec.uninsert('SK_E2')
@@ -16,7 +16,7 @@ def chirpForMulti(invar):
     f0, f1, t0, Fs, delay = 0.5, 20, 20, 1000, 5 # for looking at bimodal leading phase response in Hay cell
     I, t = getChirp(f0, f1, t0, amp, Fs, delay)
     print('running chirp on ' + str(sec))
-    applyChirp(I, t, sec, pt_cell.soma[0](0.5), t0, delay, Fs, f1, out_file_name=filename)
+    applyChirp(I, t, seg, pt_cell.soma[0](0.5), t0, delay, Fs, f1, out_file_name=filename)
 
 data = [[0, 0.5, '/u/craig/L5PYR_Resonance/timeDomainOutput/chirpSKE2/apic0_0.5'],
         [2, 0.25, '/u/craig/L5PYR_Resonance/timeDomainOutput/chirpSKE2/apic2_2.5'],

7e802460e22a0adb117817e8e14a9e6c4e21f3fb
kelley92@github.com <kelley92@bu.edu>
2020-06-17 09:55:27 -0400

    trying chirp on modified pt cell

---
+++ modifyPTcell.py
@@ -0,0 +1,62 @@
+from getCells import NeymotinCell
+cell = NeymotinCell()
+
+cfg = {'ihGbar' : 1.0,  # multiplicative factor for ih gbar in PT cells\
+        'ihGbarBasal' : 1.0, # 0.1 # multiplicative factor for ih gbar in PT cells
+        'ihlkc'           : 0.2, # ih leak param (used in Migliore)
+        'ihlkcBasal'      : 1.0,
+        'ihlkcBelowSoma'  : 0.01,
+        'ihlke'           : -86,  # ih leak param (used in Migliore)
+        'ihSlope'         : 14*2,
+        'somaNa'      : 5,
+        'dendNa'      : 0.3,
+        'axonNa'      : 7,
+        'axonRa'      : 0.005,
+        'gpas' : 0.5,  # multiplicative factor for pas g in PT cells
+        'epas' : 0.9}  # multiplicative factor for pas e in PT cells
+
+from neuron import h
+
+for sec in h.allsec():
+    for seg in sec.allseg():
+        try:
+            seg.hd.gbar = seg.hd.gbar * cfg['IhGbar']
+            seg.hd.clk = cfg['ihlkc']
+            seg.hd.elk = cfg['ihlkc']
+        except:
+            pass
+
+for sec in cell.dend:
+    for seg in sec.allseg():
+        seg.hd.gbar = seg.hd.gbar * cfg['ihGbarBasal']
+        seg.hd.clk = seg.hd.clk * cfg['ihlkcBelowSoma']
+
+for seg in cell.soma.allseg():
+    seg.hd.clk = seg.hd.clk * cfg['ihlkcBelowSoma']
+
+for sec in cell.dend:
+    for seg in cell.allseg():
+        seg.nax.gbar = 0.0153130368342 * cfg['dendNa']
+        seg.pas.e = seg.pas.e * cfg['epas']
+        seg.pas.g = seg.pas.g * cfg['gpas']
+
+for sec in cell.apic:
+    for seg in cell.allseg():
+        seg.nax.gbar = 0.0153130368342 * cfg['dendNa']
+        seg.pas.e = seg.pas.e * cfg['epas']
+        seg.pas.g = seg.pas.g * cfg['gpas']
+
+for seg in cell.soma.allseg():
+    seg.nax.gbar = 0.0153130368342  * cfg['somaNa']
+
+for seg in cell.axon.allseg():
+    seg.nax.gbar = 0.0153130368342  * cfg['axonNa']
+    seg.geom.Ra = 137.494564931 * cfg['axonRa'] #0.005 
+
+from chirpUtils import applyChirp, getChirp
+amp = 0.0025
+f0, f1, t0, Fs, delay = 0.5, 50, 50, 1000, 12 # for looking at bimodal leading phase response in Hay cell
+I, t = getChirp(f0, f1, t0, amp, Fs, delay)
+seg = cell.apic[26](0.5)
+print('running chirp on ' + str(seg))
+out = applyChirp(I, t, seg, cell.soma[0](0.5), t0, delay, Fs, f1, out_file_name='/u/craig/L5PYR_Resonance/Neymotin/testPTadjust')
